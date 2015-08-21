d2e2bf040a7652741b883779635ab8a3f6deaaee
Sarvex Jatasra <sarvex.jatasra@gmail.com>
2015-08-21 19:07:13 +0530

    Initial Commit

---
+++ Business/CarRental.Business.Entities/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Business.Entities
+{
+    [DataContract]
+    public class Car
+    {
+        [DataMember]
+        public int CarId { get; set; }
+    }
+}
