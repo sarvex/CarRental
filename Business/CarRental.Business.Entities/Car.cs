0d51f57482e0d6bdc7e07f8d572c264a7a3f7c49
Sarvex Jatasra <sarvex.jatasra@gmail.com>
2015-08-21 20:31:13 +0530

    Initial Structure

--- Business/CarRental.Business.Entities/Car.cs
+++ Business/CarRental.Business.Entities/Car.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
+using Core.Common.Core;
+using Core.Common.Contracts;
 
 namespace CarRental.Business.Entities
 {
     [DataContract]
-    public class Car
+    public class Car : EntityBase, IIdentifiableEntity
     {
         [DataMember]
         public int CarId { get; set; }
+
+        [DataMember]
+        public string Description { get; set; }
+
+        [DataMember]
+        public string Color { get; set; }
+
+        [DataMember]
+        public int Year { get; set; }
+
+        [DataMember]
+        public decimal RentalPrice { get; set; }
+
+        [DataMember]
+        public bool CurrentlyRented { get; set; }
     }
 }
--- Client/CarRental.Client.Entities/Car.cs
+++ Client/CarRental.Client.Entities/Car.cs
@@ -8,5 +8,47 @@ namespace CarRental.Client.Entities
 {
     public class Car
     {
+        private int _carId;
+        private string _description;
+        private string _color;
+        private int _year;
+        private decimal _rentalPrice;
+        private bool _currentlyRented;
+
+        public int CarId
+        {
+            get { return _carId; }
+            set { _carId = value; }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        public string Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+
+        public int Year
+        {
+            get { return _year; }
+            set { _year = value; }
+        }
+
+        public decimal RentalPrice
+        {
+            get { return _rentalPrice; }
+            set { _rentalPrice = value; }
+        }
+
+        public bool CurrentlyRented
+        {
+            get { return _currentlyRented; }
+            set { _currentlyRented = value; }
+        }
     }
 }
